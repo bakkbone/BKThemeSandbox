1dbeb4c5d550e5d25d3eeb57d69e944c6517f945
BAKKBONE Australia <hosting@bakkbone.com.au>
2022-06-25 17:31:54 +0800

    Add branding

--- _pgexport/index.php
+++ _pgexport/index.php
@@ -7,7 +7,15 @@
                 <img src="<?php echo PG_Image::getUrl( get_theme_mod( 'custom_logo', 'https://pinegrow.com/placeholders/img17.jpg' ), 'full' ) ?>" style="max-width: 350px; margin: 5px;" class="pg-empty-placeholder" href="<?php echo esc_url( home_url() ); ?>"/>
             <?php endif; ?>
         </div>
-        <div class="pg-empty-placeholder"></div>
+        <div>
+            <nav>
+                <?php wp_nav_menu( array(
+                        'menu' => 'primary',
+                        'menu_class' => 'pg-empty-placeholder',
+                        'container' => ''
+                ) ); ?>
+            </nav>
+        </div>
     </section>
 </header>        
 
